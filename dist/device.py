c356dfd55cd7c9ff409e88889780d313c06439d5
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-08-13 14:28:23 +0800

    Update device.py

--- dist/device.py
+++ dist/device.py
@@ -1,11 +1,11 @@
 import torch
 
 
-def get_devices(device_type='GPU'):
+def get_devices(device_type='cuda'):
     devices = []
-    if device_type == 'GPU':
+    if device_type == 'cuda':
         device_num = torch.cuda.device_count()
-    elif device_type == 'CPU':
+    elif device_type == 'cpu':
         device_num = torch.cpu.device_count()
     else:
         raise ValueError(f" {device_type} is not supported ")
@@ -13,16 +13,16 @@ def get_devices(device_type='GPU'):
         devices.append(torch.cuda.get_device_name(i))
     return devices
 
-def get_visible_devices(visible_device_indices:list, device_type='GPU'):
+def get_visible_devices(visible_device_indices:list, device_type='cuda'):
     visible_devices = []
     devices = get_devices(device_type=device_type)
     if len(visible_device_indices) > len(devices):
         raise RuntimeError(f" assigned devices exceed existing devices ")
-    if device_type == 'GPU':
+    if device_type == 'cuda':
         for i in visible_device_indices:
             visible_devices.append(devices[i])
         # print(': ', torch.cuda.current_device())  # torch.cuda.current_device() returns '0'
-    elif device_type == 'CPU':
-        visible_devices.append('CPU')
+    elif device_type == 'cpu':
+        visible_devices.append('cpu')
     print('current devices: ', visible_devices, f'   ({device_type})')
     return visible_devices
